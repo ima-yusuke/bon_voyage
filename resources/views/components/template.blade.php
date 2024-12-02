8e9d423ca3643597780c7443c883c8e6be2484b8
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-12-02 16:26:41 +0900

    fix：Google AnalyticsとHeader・Footer componentの記載があったが前回のプロジェクトコードの残りのため削除。

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -7,19 +7,8 @@
     @vite(['resources/css/'.$css])
 </x-head>
 <body class="bg-main-bg h-full flex flex-col">
-<x-header></x-header>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-1SS3MSY633"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-1SS3MSY633');
-</script>
 <main class="flex-1">
     {{ $slot }}
 </main>
-<x-footer></x-footer>
 </body>
 </html>
